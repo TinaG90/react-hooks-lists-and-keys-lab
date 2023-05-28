94393fb55f1cfb60ebe8ee9f46f863eccc1e4927
Altenia Gaines <gaines.tina90@gmail.com>
2023-05-27 19:45:42 -0700

    Lab complete

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,13 +2,22 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
-  return (
-    <div id="projects">
-      <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
-    </div>
-  );
+  const newProjects = projects.map((obj) => {
+    return (
+      <div id="projects" key={obj.id}>
+        <h2>My Projects</h2>
+        <div id="project-list">
+          <ProjectItem
+            id={obj.id}
+            name={obj.name}
+            about={obj.about}
+            technologies={obj.technologies}
+          />
+        </div>
+      </div>
+    );
+  });
+  return newProjects;
 }
 
 export default ProjectList;
